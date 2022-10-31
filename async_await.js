f78d4fd22544a45ddb1529e0036ec2c5c3f85785
farhadh <farhad.21.7@gmail.com>
2022-10-31 17:27:57 +0100

    Brought main() into global scope

--- async_await.js
+++ async_await.js
@@ -1,28 +1,36 @@
+console.log("Start");
+
 function sleep(ms) {
     console.log('sleep function called')
     return new Promise(r => setTimeout(() => {
       console.log(`done waiting for ${ms} ms`)
       r()
     }, ms))
-  }
+}
+
+async function doSomeAsyncStuff1() {
+  console.log('111 before sleeping')
+  await sleep(1000)
+  console.log('111 done sleeping')
+}
+
+async function doSomeAsyncStuff2() {
+  console.log('222 before sleeping')
+  await sleep(2000)
+  console.log('222 done sleeping')
+}
+
+async function main() {
+  console.log("main() called");
   
-  async function doSomeAsyncStuff1() {
-    console.log('111 before sleeping')
-    await sleep(2000)
-    console.log('111 done sleeping')
-  }
+  console.log("doSomeAsyncStuff1() called");
+  await doSomeAsyncStuff1()
   
-  async function doSomeAsyncStuff2() {
-    console.log('222 before sleeping')
-    await sleep(4000)
-    console.log('222 done sleeping')
-  }
+  console.log("doSomeAsyncStuff2() called");
+  doSomeAsyncStuff2()
   
-  async function main() {
-    doSomeAsyncStuff1()
-    doSomeAsyncStuff2()
-    console.log('here')
-  }
-  
-  main()
-  console.log('i am here now')
+  console.log('end of main()')
+}
+
+main()
+console.log('The End')
