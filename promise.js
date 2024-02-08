da3803ee2fd9a33281fb4f222e592ee1b123eda7
farhadh <farhad.21.7@gmail.com>
2024-02-08 10:31:07 +0100

    Formatting

--- promise.js
+++ promise.js
@@ -1,26 +1,26 @@
 /*
 In JavaScript promises, the `resolve` and `reject` functions are callback functions that are passed as parameters to the executor function when a new promise is created. The executor function takes two parameters: `resolve` and `reject`.
 
-The resolve function is called when the asynchronous operation is successful and returns a value that has been set for `resolve` method's signature. The `reject` function is called when the asynchronous operation fails and returns an error.
+The `resolve` function is called when the asynchronous operation is successful and returns a value that has been set for the `resolve` method's signature. The `reject` function is called when the asynchronous operation fails and returns an error.
 
-Both resolve and reject functions can take a single argument, which represents the value of the resolved or rejected promise respectively. This argument can be any valid JavaScript value, including a variable.
+Both `resolve` and `reject` functions can take a single argument, which represents the value of the resolved or rejected promise respectively. This argument can be any valid JavaScript value, including a variable.
 
 The `resolve` function in the promise constructor is simply a function you call to let the runtime know the promise has completed and the value is available.
 
-Resolve is analogous to return in standard functions. Hence, you can pass any value at all into resolve.
+Resolve is analogous to return in standard functions. Hence, you can pass any value at all into `resolve`.
 
 To resolve a promise simply means to indicate that the action has completed and the value is available.
 
-The magic is that what you pass to resolve is threaded through into the function you give to then. So internally, your then function "registers" itself with the promise and (also internally) when you call resolve this "triggers" the function you pass to then.
+The magic is that what you pass to resolve is threaded through into the function you give to `then`. So internally, your `then` function "registers" itself with the promise and (also internally) when you call `resolve` this "triggers" the function you pass to `then`.
 
-So there's a sort of relationship, which you don't need to worry about as a consumer of promises, between resolve and the then function. Think of them like creating an event handler - the bit in then is the handler and resolve is the thing that kicks off the event.
+So there's a sort of relationship, which you don't need to worry about as a consumer of promises, between `resolve` and the `then` function. Think of them like creating an event handler - the bit in `then` is the handler and `resolve` is the thing that kicks off the event.
 
 
-You use resolve reject when creating a promise yourself by using new Promise(). When you call resolve inside the promise callback, you 'resolve' that promise, meaning that it has completed successfully. If you call reject inside the promise callback, you 'reject' that promise, meaning that it has come up with an error when trying to do whatever is inside.
+You use resolve reject when creating a promise yourself by using new Promise(). When you call `resolve` inside the promise callback, you 'resolve' that promise, meaning that it has completed successfully. If you call reject inside the promise callback, you 'reject' that promise, meaning that it has come up with an error when trying to do whatever is inside.
 
-The resolve and reject parameters are only used when constructing promises, whereas for then and catch methods are used when consuming the promise (not creating it).
+The `resolve` and `reject` parameters are only used when constructing promises, whereas for then and catch methods are used when consuming the promise (not creating it).
 
-If you create your promise with the new keyword and then consume it, whatever was passed into the resolve is what is received in the then. The same applies for the reject and catch.
+If you create your promise with the new keyword and then consume it, whatever was passed into the `resolve` is what is received in the then. The same applies for the `reject` and `catch`.
 
 
 const promise = new Promise((resolve, reject) => {
